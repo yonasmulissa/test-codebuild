bb7ef863b3ededeb776f92a9be152b0c07089148
yonasm <yonas.mulissa@padi.com>
2019-08-16 13:36:18 -0700

    Added codebuild demo project

---
+++ src/main/java/com/example/codeBuildDemo/CodeBuildDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeBuildDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBuildDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBuildDemoApplication.class, args);
+	}
+
+}
